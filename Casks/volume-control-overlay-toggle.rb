cdddf56757985839d4a843cf9449f2db8a3e4a03
George Mastro <g.mastro@netstudio.gr>
2025-04-26 22:02:04 +0300

    Add postflight to handle quarantine

--- Casks/volume-control-overlay-toggle.rb
+++ Casks/volume-control-overlay-toggle.rb
@@ -7,5 +7,14 @@ cask "volume-control-overlay-toggle" do
   desc "A simple macOS menu bar app to toggle the volume control overlay"
   homepage "https://github.com/georgemastro/VolumeControlOverlayToggle"
 
+  depends_on macos: ">= :big_sur"
+
   app "VolumeControlOverlayToggle.app"
+
+  # Disable quarantine for unnotarized apps
+  postflight do
+    system_command "/usr/bin/xattr",
+                  args: ["-d", "com.apple.quarantine", "#{appdir}/VolumeControlOverlayToggle.app"],
+                  sudo: true
+  end
 end
