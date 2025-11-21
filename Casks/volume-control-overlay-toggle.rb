5e73e152881d66c4b5fc742351a752ca37bc9502
George Mastro <g.mastro@netstudio.gr>
2025-11-21 16:31:17 +0200

    Update to 1.2.2 - remove postflight script

--- Casks/volume-control-overlay-toggle.rb
+++ Casks/volume-control-overlay-toggle.rb
@@ -1,6 +1,6 @@
 cask "volume-control-overlay-toggle" do
-  version "1.2.1"
-  sha256 "d160eb9a9e00fb67c1f2111b9c54996a908f0a9440b1fe3830efc23c6dc99d48"
+  version "1.2.2"
+  sha256 "165338167736b5aab7bd578e24018c7d92facba00ebd8f366101ea9d13227904"
 
   url "https://github.com/georgemastro/VolumeControlOverlayToggle/releases/download/v#{version}/VolumeControlOverlayToggle.zip"
   name "VolumeControlOverlayToggle"
@@ -10,17 +10,4 @@ cask "volume-control-overlay-toggle" do
   depends_on macos: ">= :big_sur"
 
   app "VolumeControlOverlayToggle.app"
-
-  # Handle quarantine more gracefully
-  postflight do
-    set_permissions "#{appdir}/VolumeControlOverlayToggle.app", "0755"
-
-    quarantine_attribute = `xattr -l "#{appdir}/VolumeControlOverlayToggle.app" | grep com.apple.quarantine`
-
-    if quarantine_attribute.strip.length > 0
-      system_command "/usr/bin/xattr",
-                    args: ["-d", "com.apple.quarantine", "#{appdir}/VolumeControlOverlayToggle.app"],
-                    sudo: true
-    end
-  end
 end
